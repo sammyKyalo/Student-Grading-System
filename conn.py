c59505f94eb145073a95ba36f547cafad7b0d89b
Sammy Kyalo <sammykyalo47@gmail.com>
2024-04-11 14:05:13 +0300

    commit

--- conn.py
+++ conn.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 def get_connection():
     try:
         connection = pymysql.connect(
-            host="localhost",
+            host="192.168.0.26",
             port=3306,
             user="root",
             password="Babygal@$",
