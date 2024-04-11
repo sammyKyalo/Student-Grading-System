70cb4d62ae8574b4b88cdc252662ec2847f16301
Sammy Kyalo <sammykyalo47@gmail.com>
2024-04-11 09:48:58 +0300

    commit

--- conn.py
+++ conn.py
@@ -1,7 +1,7 @@
 import logging
 import pandas as pd
-import mysql.connector
-from mysql.connector import Error
+import pymysql
+from pymysql import Error
 from sqlalchemy import create_engine
 from urllib.parse import quote_plus
 
@@ -11,14 +11,14 @@ logger = logging.getLogger(__name__)
 
 def get_connection():
     try:
-        connection = mysql.connector.connect(
+        connection = pymysql.connect(
             host="localhost",
-            port="3306",
+            port=3306,
             user="root",
             password="Babygal@$",
             database="results"
         )
-        if connection.is_connected():
+        if connection.open:
             logger.info("Connected to MySQL database")
             return connection
     except Error as e:
@@ -28,19 +28,22 @@ def get_connection():
 def create_table_if_not_exists(connection, result, School, Grade, term, exam_type):
     if connection:
         cursor = connection.cursor()
-        table_name = f"{School}_{Grade}_{term}_{exam_type}".replace(" ", "_").lower()  # Convert table name to lowercase
+        table_name = f"{School}_{Grade}_{term}_{exam_type}".replace(" ", "_").lower()
         columns = ', '.join([f'`{column}` TEXT' for column in result.columns])
-        cursor.execute(f"CREATE TABLE IF NOT EXISTS `{table_name}` ({columns})")
-        connection.commit()
-        logger.info("Table %s created successfully", table_name)
-
+        create_table_query = f"CREATE TABLE IF NOT EXISTS `{table_name}` ({columns})"
+        try:
+            cursor.execute(create_table_query)
+            connection.commit()
+            logger.info("Table %s created successfully", table_name)
+        except Error as e:
+            logger.error("Error creating table %s: %s", table_name, e)
 
 def save_result_to_sql(result, School, Grade, term, exam_type):
     connection = get_connection()
     if connection is not None:
         try:
             password = quote_plus("Babygal@$")
-            engine = create_engine(f'mysql+mysqlconnector://root:{password}@localhost:3306/results')
+            engine = create_engine(f'mysql+pymysql://root:{password}@localhost:3306/results')
             table_name = f"{School}_{Grade}_{term}_{exam_type}".replace(" ", "_")
             create_table_if_not_exists(connection, result, School, Grade, term, exam_type)
             result.to_sql(table_name, con=engine, if_exists='append', index=False)
